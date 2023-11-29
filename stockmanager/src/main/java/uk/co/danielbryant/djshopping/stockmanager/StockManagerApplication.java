a6644408448155dfb22cec08e02db0fc724359d6
Sourav Chandel <73681422+sourav3227@users.noreply.github.com>
2023-11-29 14:42:15 +0530

    Update StockManagerApplication.java

--- stockmanager/src/main/java/uk/co/danielbryant/djshopping/stockmanager/StockManagerApplication.java
+++ stockmanager/src/main/java/uk/co/danielbryant/djshopping/stockmanager/StockManagerApplication.java
@@ -1,7 +1,11 @@
 package uk.co.danielbryant.djshopping.stockmanager;
+package com.example.springbootmanagementexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Date;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class StockManagerApplication {
@@ -9,4 +13,11 @@ public class StockManagerApplication {
     public static void main(String[] args) {
         SpringApplication.run(StockManagerApplication.class, args);
     }
+}
+@RestController
+public class SimpleRestController {
+     @GetMapping("/example")
+     public String example() {
+         return "Hello User !! " + new Date();
+     }
 }
